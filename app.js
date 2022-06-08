49c0290a09c621b24a29bab3c84c405ddf8a6b09
AbdulRehman-Irfan <101065713+AbdulRehman-Irfan@users.noreply.github.com>
2022-06-08 20:13:54 +0500

    ..

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api/users');
-var mobilesRouter = require('./routes/api/mobiles');
-var config = require('config');
-var cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/api/users");
+var mobilesRouter = require("./routes/api/mobiles");
+var config = require("config");
+var cors = require("cors");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/mobiles', mobilesRouter);
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/mobiles", mobilesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
-app.use(cors(
-  {
-    origin: "*",
-  }
-));
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-mongoose.connect(config.get("db"), { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect(config.get("db"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 module.exports = app;
